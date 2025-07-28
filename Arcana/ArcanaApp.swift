f7aee2737b931d2cf07b5999e2154e3da147585b
incidentallycilantro <derskine25@proton.me>
2025-07-28 17:05:06 -0400

    Initial Commit

---
+++ Arcana/ArcanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcanaApp.swift
+//  Arcana
+//
+//  Created by Dylan Erskine on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ArcanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcanaTests/ArcanaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArcanaTests.swift
+//  ArcanaTests
+//
+//  Created by Dylan Erskine on 7/28/25.
+//
+
+import Testing
+@testable import Arcana
+
+struct ArcanaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
